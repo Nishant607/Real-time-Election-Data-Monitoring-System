d5cd65f229a82b0bf96f10be397255da66dcd757
Nishant607 <nishantchauhan1084@gmail.com>
2026-02-23 17:54:32 +0530

    Dashboard update and vote summary table fix

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import dashboard,alerts_page
 
+
 urlpatterns = [
     path("", dashboard, name="dashboard"),
-    path("alerts/",alerts_page, name="alerts"),
-
+    path("alerts/",alerts_page, name="alerts"),\
 ]
--- core/views.py
+++ core/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render
+from .models import Election, VoteRecord, Candidate, Alert
 from django.db.models import Sum
-from .models import Election, VoteRecord, Candidate, Anomaly, Alert
-
-from django.shortcuts import render
-from .models import Election, VoteRecord, Candidate, Anomaly, Alert
-from django.db.models import Sum
-
 
 def dashboard(request):
+    elections = Election.objects.all()
+    selected_election_id = request.GET.get('election')
+
+    if selected_election_id:
+        selected_election = Election.objects.get(id=selected_election_id)
+        candidates = Candidate.objects.filter(election=selected_election)
+        vote_records = VoteRecord.objects.filter(candidate__election=selected_election)
+    else:
+        candidates = Candidate.objects.all()
+        vote_records = VoteRecord.objects.all()
+
+    total_votes = vote_records.aggregate(total=Sum('votes'))['total'] or 0
+
     candidate_votes = (
-        VoteRecord.objects
+        vote_records
         .values('candidate__name', 'candidate__party')
         .annotate(total_votes=Sum('votes'))
-        .order_by('-total_votes')
     )
+
     context = {
-        'elections': Election.objects.count(),
-        'candidates': Candidate.objects.count(),
-        'votes': VoteRecord.objects.aggregate(total=Sum('votes'))['total'] or 0,
+        'elections': elections.count(),
+        'elections_list': elections,
+        'selected_election_id': int(selected_election_id) if selected_election_id else None,
+        'candidates': candidates.count(),
+        'votes': total_votes,
         'alerts': Alert.objects.filter(status='Active').count(),
         'recent_alerts': Alert.objects.order_by('-created_at')[:5],
         'candidate_votes': candidate_votes
@@ -26,9 +36,6 @@ def dashboard(request):
     return render(request, 'dashboard.html', context)
 
 
-
 def alerts_page(request):
     alerts = Alert.objects.all().order_by('-created_at')
     return render(request, 'alerts.html', {'alerts': alerts})
-
-
